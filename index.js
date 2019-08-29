bce4c227b77f0b7638ed7fc21aa0e58b3b35770f
David Botero Salazar <david.botero@PRAGMA.COM.CO>
2019-08-29 08:06:26 -0500

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 var auth = { apiKey: 'Lum/xCtzlsE/TskMv12rBLRN78Lo998VUwEl0nZsKvUv+lF2deMMU7xKeur0XYlMiGElj4KGaw3z1/O8jndJ+Ol3mA9g9+//Adzc5zQJJAwzWSy4lGmqCpfF17zZ5dTKKYJo8C2OEyfqEbZG1Bfqeg==' };
 var api = new createsend(auth);
-var listId = '673859f847e17c44ee82782da616257e' // The ID of the list
+var listId = 'd41bdfb6c09b28460bb3b2fd088cbe5e' // The ID of the list
 
 
 app.post("/send-user", (req, res) => {
@@ -24,7 +24,27 @@ app.post("/send-user", (req, res) => {
         EmailAddress: req.body.email,
         Name: req.body.name,
         CustomFields: [
-            { Key: 'pleasures', Value: req.body.pleasures }
+            { Key: 'LaTranquilidadDeLaNaturaleza', Value: req.body.LaTranquilidadDeLaNaturaleza },
+            { Key: 'IraCineaVerUnaBuenaPelicula', Value: req.body.IraCineaVerUnaBuenaPelicula },
+            { Key: 'LaMusicaMeEncantaEnTodasSusFormas', Value: req.body.LaMusicaMeEncantaEnTodasSusFormas },
+            { Key: 'AprenderDeTodoLoQuePueda', Value: req.body.AprenderDeTodoLoQuePueda },
+            { Key: 'EstarActivoElDeporteMeSubeElAnimo', Value: req.body.EstarActivoElDeporteMeSubeElAnimo },
+            { Key: 'UnaBuenaLectura', Value: req.body.UnaBuenaLectura },
+            { Key: 'ConMisAmigos', Value: req.body.ConMisAmigos },
+            { Key: 'ConMiFamilia', Value: req.body.ConMiFamilia },
+            { Key: 'ConMisMascotas', Value: req.body.ConMisMascotas },
+            { Key: 'ConPersonasQueVoyConociendoEnMiDiaDia', Value: req.body.ConPersonasQueVoyConociendoEnMiDiaDia },
+            { Key: 'RealmenteNoDependoDeNadie', Value: req.body.RealmenteNoDependoDeNadie },
+            { Key: 'Meditacion', Value: req.body.Meditacion },
+            { Key: 'UnaNuevaTecnicaDeBaile', Value: req.body.UnaNuevaTecnicaDeBaile },
+            { Key: 'Cocinar', Value: req.body.Cocinar },
+            { Key: 'Ingles', Value: req.body.Ingles },
+            { Key: 'Computacion', Value: req.body.Computacion },
+            { Key: 'Artes', Value: req.body.Artes },
+            { Key: 'CuidarDeMiSalud', Value: req.body.CuidarDeMiSalud },
+            { Key: 'TenerUnHogarParMiyMiFamilia', Value: req.body.TenerUnHogarParMiyMiFamilia },
+            { Key: 'UtilizarMiTiempoEnActividadesQueMeGustan', Value: req.body.UtilizarMiTiempoEnActividadesQueMeGustan },
+            { Key: 'QueTeGustariaEncontrarEnComfama', Value: req.body.QueTeGustariaEncontrarEnComfama }
           ]
     };
     api.subscribers.addSubscriber(listId, user, (err, response) => {
@@ -40,6 +60,7 @@ app.post("/send-user", (req, res) => {
     });
 
 })
+
 
 app.listen(port, () => {
     console.log("Servidor inicilizado en ", port)
