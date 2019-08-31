1efa409679543101636d5773c90d24cc60dbb43e
David Botero Salazar <david.botero@PRAGMA.COM.CO>
2019-08-31 08:06:42 -0500

    image

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var createsend = require('createsend-node');
 const hbs = require('hbs');
-
-
-
 const app = express();
+
+app.use(express.static(__dirname+'/views'));
+
 app.set('view engine','hbs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
